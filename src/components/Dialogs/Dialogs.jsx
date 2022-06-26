0e2f501703d1ab6b46626d9ad517dbec5eed37e8
Dmitriy Fedoseev <dmsr.fedoseev@gmail.com>
2022-06-26 18:57:45 +0300

    Added a list of names and messages to the Dialogs page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
-/* import d from './Dialog.module.css'; */
+import s from './Dialogs.module.css';
 
 const Dialog = (props) => {
     return (
-        <div>
-             
+        <div className={s.dialogs}>
+             <div className={s.dialogs_items}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Антон
+                </div>
+                <div className={s.dialog}>
+                    Павел
+                </div>
+                <div className={s.dialog}>
+                    Саша
+                </div>
+                <div className={s.dialog}>
+                    Света
+                </div>
+                <div className={s.dialog}>
+                    Оля
+                </div>
+             </div>
+             <div className={s.messages}>
+                <div className={s.message}>Привет</div>
+                <div className={s.message}>Как жизнь молодая?</div>
+                <div className={s.message}>У меня тоже все хорошо.</div>
+             </div>
         </div>
     )
 }
